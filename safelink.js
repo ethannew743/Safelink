2ca7cb048081958eec4537ce3bba1d4bdfe260ed
ethannew743 <56137343+ethannew743@users.noreply.github.com>
2019-10-04 05:41:38 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="Naruto eps 500.html"}
-if(x=="2"){xxx="Naruto eps 400.html"}
-if(x=="3"){xxx="Naruto eps 423.html"}
-if(x=="4"){xxx="Naruto eps 424"}a_to_ve[i].href="https://namianime2019.blogspot.com/2019/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="naruto-eps-500.html"}
+if(x=="2"){xxx="naruto-eps-400.html"}
+if(x=="3"){xxx="naruto-eps-423.html"}
+if(x=="4"){xxx="naruto-eps-424.html"}a_to_ve[i].href="https://namianime2019.blogspot.com/2019/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
